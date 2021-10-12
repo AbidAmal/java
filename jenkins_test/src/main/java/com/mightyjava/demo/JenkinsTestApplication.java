9b1174c6c1c487ffde0e6717e91c662252ec0141
AbidAmal <80819768+AbidAmal@users.noreply.github.com>
2021-10-12 14:30:33 +0100

    Add files via upload

---
+++ jenkins_test/src/main/java/com/mightyjava/demo/JenkinsTestApplication.java
@@ -0,0 +1,14 @@
+package com.mightyjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("helloooooo");
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+}
